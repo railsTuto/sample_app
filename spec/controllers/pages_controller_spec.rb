ddcd8bcdcdbc8e8f7c74eae60da08fb3c84a5753
test test <null@gmail.com>
2014-02-17 00:48:25 +0100

    end with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have good title" do
+      get 'home'
+      response.should have_selector("title", :content => "Sample App for Rails Tuto | Home")
     end
   end
 
@@ -14,6 +20,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have good title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Sample App for Rails Tuto | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have good title" do
+      get 'about'
+      response.should have_selector("title", :content => "Sample App for Rails Tuto | About")
+    end
+
   end
 
 end
