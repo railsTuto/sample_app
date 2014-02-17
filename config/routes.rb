d86e105199da0a2f537d69907db3a99666ff9718
test test <null@gmail.com>
2014-02-17 23:38:35 +0100

    add css + link/route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  
+  get "users/new"
+  # Get Static Page
+  # get "pages/home"
+  # get "pages/contact"
+  # get "pages/about"
+  # get "pages/help"
+
+  match '/contact', :to => 'pages#contact', :via => :get
+  match '/about', :to => 'pages#about', :via => :get
+  match '/help', :to => 'pages#help', :via => :get
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
