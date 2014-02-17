0962193f5f8b39c370246ef2233f4c8a842bd0c5
test test <null@gmail.com>
2014-02-17 02:34:21 +0100

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # return title based on page
+  def title
+    base_title = "Sample App for Rails Tuto"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
