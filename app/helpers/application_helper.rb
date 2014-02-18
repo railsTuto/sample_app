b620fbdb5ada9d63fe7801e3c205dd9134aff194
test test <null@gmail.com>
2014-02-18 01:21:07 +0100

    add test for link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # return logo
+  def logo
+    image_tag("logo.png", :alt => "Example Application", :class => "round")
+  end
+
   # return title based on page
   def title
     base_title = "Sample App for Rails Tuto"
